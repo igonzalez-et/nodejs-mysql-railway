dbb51c7ae3baef3d196c460253898735d85da321
igonzalez-et <igonzalezvirsa@gmail.com>
2023-04-17 16:30:45 +0200

    first commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.DB_HOST || 3000;
 
 export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_HOST || "igonzalez";
-export const DB_PASS = process.env.DB_HOST || "Superlocal123";
-export const DB_NAME = process.env.DB_HOST || "testnodejs";
-export const DB_PORT = process.env.DB_HOST || 3306;
+export const DB_USER = process.env.DB_USER || "igonzalez";
+export const DB_PASS = process.env.DB_PASS || "Superlocal123";
+export const DB_NAME = process.env.DB_NAME || "testnodejs";
+export const DB_PORT = process.env.DB_PORT || 3306;
